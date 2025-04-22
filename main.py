d6fe3d5830c242b48d30227243e84f56d28a0871
Hunter Hancock <hancock.hunter@gmail.com>
2025-04-21 19:13:23 -0500

    wait for ready to update events

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ intents = discord.Intents.default()
 intents.guild_scheduled_events = True
 
 client = discord.Client(intents=intents)
-guild = client.get_guild("the_guild_id")
+guild: discord.Guild = None
 
 @client.event
 async def on_ready():
+	global guild
 	print(f'We have logged in as {client.user}')
+	guild = client.get_guild("the_guild_id")
+	update_events()
 
 # client.run(os.getenv('DISCORD_TOKEN'))
 
 ddb = aws.DynamoDBClient()
 
-print(guild.scheduled_events)
-on_meetup = events.fetch_meetup_events()
-for e in on_meetup:
-	table_item = ddb.read_item(e.id, e.sort)
-	if not table_item:
-		ddb.write_item(e)
+async def update_events():
+	print(guild.scheduled_events)
+	on_meetup = events.fetch_meetup_events()
+	for e in on_meetup:
 		table_item = ddb.read_item(e.id, e.sort)
+		if not table_item:
+			ddb.write_item(e)
+			table_item = ddb.read_item(e.id, e.sort)
